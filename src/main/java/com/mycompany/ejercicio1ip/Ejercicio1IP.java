2b9fc837760d8f4cede3a870b6bdd0cb4dd2b681
Dell <Dell@DESKTOP-J5Q3ESE>
2024-11-03 21:49:53 -0600

    TareaModulo6

---
+++ src/main/java/com/mycompany/ejercicio1ip/Ejercicio1IP.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.ejercicio1ip;
+//Copyright:Ruth Bautista(202320050047).
+//Este programa imprime un mensaje de saludo en la consola.
+//mostrando cómo definir una clase y un metodo principal.
+public class Ejercicio1IP {
+
+    public static void main(String[] args) {
+        System.out.println("Hola, soy Ruth Bautista");
+    }
+}
